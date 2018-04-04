a02964a03303d124a42f33b7a0e8e35fc76833d0
LuoPK <luopeike0911@163.com>
2018-04-04 15:35:32 +0800

    1. OC 项目新增 Swift 桥接文件。

---
+++ ShecareSDKDemo-OC/ShecareSDKDemo-OC/Demo.swift
@@ -0,0 +1,14 @@
+//
+//  Demo.swift
+//  ShecareSDKDemo-OC
+//
+//  Created by 罗培克 on 2018/4/4.
+//  Copyright © 2018年 北京爱康泰科技有限责任公司. All rights reserved.
+//
+
+import UIKit
+
+///  用于防止 Objective-C 项目不加载 Swift 运行库
+class Demo: NSObject {
+
+}
